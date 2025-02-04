01525762ab6622fc5c7fa1598ed50e4fc19761ce
Gilberto Echeverria <gilbertoef@gmail.com>
2025-02-04 09:58:08 -0600

    Add simple platformer from tutorial

---
+++ 03_DOM_Game/js/game_classes.js
@@ -0,0 +1,33 @@
+/*
+ * General classes that can be useful for a game
+ *
+ * Gilberto Echeverria
+ * 2025-01-22
+ */
+
+"use strict";
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(other) {
+        return new Vec(this.x + other.x, this.y + other.y);
+    }
+
+    minus(other) {
+        return new Vec(this.x - other.x, this.y - other.y);
+    }
+
+    times(factor) {
+        return new Vec(this.x * factor, this.y * factor);
+    }
+
+    get length() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+}
+
+console.log(new Vec(4, 3).length);
